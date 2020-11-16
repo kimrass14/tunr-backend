52533f8d5c8aefb4ec7adeb48e0658a4a46f118e
Kim Rasmussen <68238123+kimrass14@users.noreply.github.com>
2020-11-16 10:10:14 -0600

    crud routes tested in postman

---
+++ db/migrate/20201116153839_create_songs.rb
@@ -0,0 +1,11 @@
+class CreateSongs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :songs do |t|
+      t.string :artist
+      t.string :title
+      t.string :time
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Song.create([
+    { artist: "John Legend", title: "Wild", time: "3:16" }, 
+    { artist: "Tones And I", title: "Fly Away", time: "2:58" },
+    { artist: "Aloe Blacc", title: "All Love Everything", time: "2:27" },
+    { artist: "Sam Smith", title: "Kids Again", time: "3:27" },
+    { artist: "Sia", title: "Courage to Change", time: "4:52" },
+    { artist: "John K", title: "parachute", time: "2:36" },
+])
